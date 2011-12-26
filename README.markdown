9608a4c200e006cbf756211354347eda320426c5
mark <mark@mark-dell-laptop.(none)>
2011-12-25 23:33:33 -0500

    Updated README

---
+++ README.markdown
@@ -0,0 +1,5 @@
+Prayer App
+
+This is a port of my markmcfadden.net/PrayerWeb app that I have created in .Net 2.0 back in 2005 to Rails 3. 
+
+I am using this to better learn Rails 3.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrayerApp::Application.config.secret_token = 'ac7c58a28c5fc3be68c9c0955cd9b9c63a957d9e2014bf7650d887fedf7873414790ce269a8b4f155b9ae5579d625cc1302be7c6d2a5716491022ebd8c170122'
