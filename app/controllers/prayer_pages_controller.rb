36e6e368e9195625d48d039deed4d121ebdd3adb
Mark McFadden <m2web@yahoo.com>
2011-12-26 20:47:58 -0500

    updated prayer-pages.home action

--- app/controllers/prayer_pages_controller.rb
+++ app/controllers/prayer_pages_controller.rb
@@ -1,5 +1,17 @@
 class PrayerPagesController < ApplicationController
+require 'net/http'
+
   def home
+	options = ["&include-short-copyright=0",
+		"output-format=plain-text",
+		"include-passage-horizontal-lines=0",
+		"include-heading-horizontal-lines=0",
+		"include-footnotes=false",
+		#"include-passage-references=false",
+		"include-short-copyright=false",
+		"include-content-type=false"].join("&")
+	base_url = "http://www.esvapi.org/v2/rest/dailyVerse?key=IP"    
+	@todaysVerse = Net::HTTP.get(::URI.parse(base_url+options))
   end
 
   def today
