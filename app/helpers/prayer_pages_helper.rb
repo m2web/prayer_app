b8944ae29ccaac9827a42633d8eff40687d9c931
Mark McFadden <m2web@yahoo.com>
2011-12-27 16:03:36 -0500

    updated prayer pages home

--- app/helpers/prayer_pages_helper.rb
+++ app/helpers/prayer_pages_helper.rb
@@ -1,2 +1,14 @@
 module PrayerPagesHelper
+	def todaysVerse
+		options = [
+			#"output-format=plain-text",
+			"include-passage-horizontal-lines=0",
+			"include-heading-horizontal-lines=0",
+			"include-footnotes=false",
+			#"include-passage-references=false",
+			#"include-short-copyright=false",
+			"include-content-type=false"].join("&")
+		base_url = "http://www.esvapi.org/v2/rest/dailyVerse?key=IP&"    
+		Net::HTTP.get(::URI.parse(base_url+options))
+	end
 end
