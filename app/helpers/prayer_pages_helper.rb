b56ae2fcf15199ec0dd711bb6b078bf8e9e0e4d2
Mark McFadden <m2web@yahoo.com>
2011-12-28 11:51:23 -0500

    updates for custom helper classes and the views of the class output

--- app/helpers/prayer_pages_helper.rb
+++ app/helpers/prayer_pages_helper.rb
@@ -1,15 +1,15 @@
 module PrayerPagesHelper
-require 'net/http'
+require 'customClasses/esv_daily_verse'
+require 'customClasses/esv_passage'
+
 	def todaysVerse
-		options = [
-			#"output-format=plain-text",
-			"include-passage-horizontal-lines=0",
-			"include-heading-horizontal-lines=0",
-			"include-footnotes=false",
-			#"include-passage-references=false",
-			#"include-short-copyright=false",
-			"include-content-type=false"].join("&")
-		base_url = "http://www.esvapi.org/v2/rest/dailyVerse?key=IP&"    
-		Net::HTTP.get(::URI.parse(base_url+options))
+		bible = EsvDailyVerse.new
+		bible.getVerse()
 	end
+
+	def getESVPassage(passage)
+		bible = EsvPassage.new
+		bible.doPassageQuery(passage)
+	end
+
 end
