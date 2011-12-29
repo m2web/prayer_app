dda30c99f2dbb6b04982694896b039acef751963
Mark McFadden <m2web@yahoo.com>
2011-12-29 01:24:10 -0500

    Addition of the app_config.yml and its use in the prayer pages helper class

--- app/helpers/prayer_pages_helper.rb
+++ app/helpers/prayer_pages_helper.rb
@@ -15,8 +15,8 @@ require 'customClasses/esv_passage'
 	def getMemoryVerses
 		bible = EsvPassage.new
 		output = ""
-		yearsVersesArray = [ "Matthew 6:33", "John 1:1", "Genesis 1:1", "Galatians 2:20", "2 Timothy 3:16", "Psalms 1:1-2", "Proverbs 21:1", "Romans 1:19-20", "Ephesians 1:3-6", "Revelation 7:9-10", "James 4:7-8", "John 1:14" ]
-		verseArray = yearsVersesArray[0..Time.now.month-1]
+		#The APP_CONFIG['yearsVersesArray'] is set in the /config/app_config.yml
+		verseArray =  APP_CONFIG['yearsVersesArray'][0..Time.now.month-1]
 		verseArray.reverse_each {|x| 
 			output += bible.doPassageQuery(x)			
 		}
