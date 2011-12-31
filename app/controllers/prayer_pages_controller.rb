95c05a87c0779ebe46f746131ab05058b5bcfcad
Mark McFadden <m2web@yahoo.com>
2011-12-31 16:54:32 -0500

    updated app_config to comply with pysch yaml parser

--- app/controllers/prayer_pages_controller.rb
+++ app/controllers/prayer_pages_controller.rb
@@ -1,6 +1,6 @@
 class PrayerPagesController < ApplicationController
 
-  def home
+	def home
 
 		#The APP_CONFIG['yearsVersesArray'] is set in the /config/app_config.yml
 		verseArray =  APP_CONFIG['yearsVersesArray'][0..Time.now.month-1]
@@ -8,7 +8,7 @@ class PrayerPagesController < ApplicationController
 	
 	end
 
-  def today
-  end
+  	def today
+  	end
 
 end
