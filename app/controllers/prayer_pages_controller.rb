2cc0125d904b005ad8ef568e7af02a71b8c7b68e
Mark McFadden <m2web@yahoo.com>
2012-01-02 17:29:34 -0500

    updated the today prayer page with the view and controller

--- app/controllers/prayer_pages_controller.rb
+++ app/controllers/prayer_pages_controller.rb
@@ -9,12 +9,65 @@ class PrayerPagesController < ApplicationController
 	end
 
 	def today
-		#get the total number of requests
+		#get today's requests to pray and trust God
+		@todaysRequests = getTodaysRequests
+		
+		#get today's Psalm to read for a reminder to adore the one true God
+		@thePsalmChapter = getTodaysPsalm
+
+	end
+	
+	#start of the private helper methods
+	private
+	
+	def getTodaysRequests
+	#get the total number of requests
 		@requests = Request.all
 		@requestsCount = @requests.length
 
+		#keep adding 1 as 0 does not act as a good divisor or multiplicand
 		#get the day number
-		@dayNumber = Time.new.wday
+		@dayNumber = Time.new.wday + 1
+
+		#categories per day = (categories / number of days of prayer w unique requests) + 1
+		@categoriesPerDay = ( @requests.length / 5) + 1;
+
+		#determine the last category to pray about that day
+		if (@dayNumber * @categoriesPerDay <= @requestsCount ) then
+			@categoryEnd = @dayNumber * @categoriesPerDay
+		else
+			#get a random number for category end since the last days of the week will have the same requests
+        if (@dayNumber > 4) then #if day greater than set day
+	        #Random random = new Random();
+          #categoryEnd = random.Next(3, numberOfCategories);
+					@categoryEnd = Random.new.rand(3..@requestsCount)
+        else #it is Monday - Thursday
+					@categoryEnd = @requestsCount;
+				end
+		end
+	
+		#determine the first category to pray about that day
+		@categoryStart = @categoryEnd - @categoriesPerDay;
+
+		@requests[@categoryStart..@categoryEnd-1] #minus 1 as it is 0 based
+
+	end
+
+	def getTodaysPsalm
+
+		#get today's Psalm to read to open prayer in adoration
+		@monthDayNumber = Time.now.mday
+		#if the 31st then set a random day
+    if (@monthDayNumber > 30) then
+    	#given that there are 150 Psalms, max day is 30
+      @monthDayNumber = Random.new.rand(1..30)
+    end
+
+		@daysLastChapterNumber = @monthDayNumber * 5
+    @daysFirstChapterNumber = @daysLastChapterNumber - 4
+
+		Random.new.rand(@daysFirstChapterNumber..@daysLastChapterNumber)
+	
 	end
 
 end
