45c5b37d7eddde3e8a3367ae9f6595c19b187f23
Mark McFadden <m2web@yahoo.com>
2012-01-16 21:05:39 -0500

    added title variable in the view erbs populated from the controller.

--- app/controllers/prayer_pages_controller.rb
+++ app/controllers/prayer_pages_controller.rb
@@ -2,6 +2,8 @@ class PrayerPagesController < ApplicationController
 
 	def home
 
+		@title = "Mark's Prayer App"
+
 		#The APP_CONFIG['yearsVersesArray'] is set in the /config/app_config.yml
 		verseArray =  APP_CONFIG['yearsVersesArray'][0..Time.now.month-1]
 		@monthlyMemoryVerses = verseArray.reverse
@@ -12,6 +14,9 @@ class PrayerPagesController < ApplicationController
 	end
 
 	def today
+
+		@title = "Today's Prayer"
+
 		#get today's requests to pray and trust God
 		@todaysRequests = getTodaysRequests
 
