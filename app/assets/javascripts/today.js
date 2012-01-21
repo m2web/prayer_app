9803fe4da0845c8b2ec0f37ac7ff1d6ccce40ee8
Mark McFadden <m2web@yahoo.com>
2012-01-20 21:49:32 -0500

    Added JavaScript timer to display on Today's view

--- app/assets/javascripts/today.js
+++ app/assets/javascripts/today.js
@@ -9,3 +9,61 @@ $(document).ready(function () {
    	})
 	});
 });
+
+//***** Today's view prayer timer functions ******
+//show button after time has run out
+function showButton(){
+	document.getElementById("resetButton").style.display = "block";
+}
+
+function tickTock(){
+	//show the time by setting it's display to block...so you don't see 0:00 right off the bat
+	document.getElementById("time").style.display = "inline";
+	//set time h1 tag in a variable called time
+	var time = document.getElementById("time");
+	//create minutes and call it mins
+	var mins = Math.floor(remainingSeconds / 60);
+	//create seconds and call it secs
+	var secs = remainingSeconds - (mins * 60);
+	//add 0 to seconds if it's less than 10...we don't want just 9, we want 09, 08, 07 and so on
+	if(secs < 10){
+		secs = "0" + secs;	
+	}
+	//set the timer to a string to display in the time h1 tag
+	var actualTime = mins.toString() + ":" + secs;
+	//display the time as a string in the time h1 tag
+	time.innerHTML = actualTime;
+	//once timer is zero, clear the interval and run the show button function
+	if(remainingSeconds == 0){
+		//stop timer
+		clearInterval(interval);
+		//run the showButton function
+		showButton();
+		//optional hide time when countdown reaches zero
+		//document.getElementById("time").style.display = "none";
+	}
+	//make seconds count down by using --
+	remainingSeconds --;
+}
+
+function startCountdown(){
+	//hide the button after clicking reset counter
+	document.getElementById("resetButton").style.display = "none";
+	//set amount of minutes for countdown
+	//****************************
+	//** Number of Minutes set here **
+	var minutes = 20.00;
+	//****************************
+	//****************************
+	//make minutes out of seconds
+	remainingSeconds = minutes * 60;
+	//run tickTock function every one second
+	interval = setInterval(tickTock, 1000);
+}
+
+//on load run startCountdown function
+window.onload = function(){
+	startCountdown();
+	document.getElementById("resetButton").onclick = startCountdown;
+}
+//***** end today's view prayer timer functions ******
