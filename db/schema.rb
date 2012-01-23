9b21d8c469a20ac9ae4929da06e21dba1d852559
Mark McFadden <m2web@yahoo.com>
2012-01-22 21:45:45 -0500

    added validation to the upcoming use of user validation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120122054225) do
+ActiveRecord::Schema.define(:version => 20120123023540) do
 
   create_table "categories", :id => false, :force => true do |t|
     t.integer  "id",         :null => false
