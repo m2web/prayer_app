7ffee95d67aa2b4130598b811aab987831b9f378
mark <m2web@yahoo.com>
2012-08-23 23:07:45 -0400

    updated config/initializers/session_store.rb and config/initializers/wrap_parameters.rb as I had too

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-PrayerApp::Application.config.session_store :cookie_store, key: '_prayer_app_session'
+#PrayerApp::Application.config.session_store :cookie_store, key: '_prayer_app_session'
+PrayerApp::Application.config.session_store :cookie_store, :key => '_prayer_app_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
