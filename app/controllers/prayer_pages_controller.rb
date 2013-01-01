5114175bddb790ee79e358460720292537c14ae5
mark <m2web@yahoo.com>
2013-01-01 09:32:11 -0500

    prayer pages controller not displaying first memory verse as the first verse is in the month of February and not January. Now fixed.

--- app/controllers/prayer_pages_controller.rb
+++ app/controllers/prayer_pages_controller.rb
@@ -112,8 +112,13 @@ class PrayerPagesController < ApplicationController
 	end
 
 	def getJMVerse
-		# the verse months are February to November but this matches them
-		verseMonthNumbers = (1..Time.now.month).to_a #to.a is a toArray command to get the range into an array i.e. [1,2,3]
+		# the verse months are February to November or 2 to 11
+		monthNumber = Time.now.month
+		#we are starting at February so make the end of the range 2
+		if monthNumber < 2
+			monthNumber = 2
+		end
+		verseMonthNumbers = (0..monthNumber).to_a #to.a is a toArray command to get the range into an array i.e. [1,2,3]
 		@jmverses = JmVerse.find(:all, :order => 'verse_month DESC', :conditions=> {:verse_month => [verseMonthNumbers]})
 
 	end
